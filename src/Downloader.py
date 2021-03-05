87c71a6c7066980f5cfef3dceda9dcc4a2ce5717
VinterSallad <alexander_huynh@icloud.com>
2021-03-05 12:10:20 +0100

    Implement working framework for downloading

--- src/Downloader.py
+++ src/Downloader.py
@@ -1,6 +1,24 @@
-from pytube import YouTube
+import pytube
 
 
 class Downloader:
 
-    def __init__(self):
+    #
+    def __init__(self, url):
+        self.youtube = pytube.YouTube(url)
+
+    #
+    def download(self, directory):
+        video = self.youtube.streams.get_highest_resolution()
+        video.download(directory)
+
+    #
+    def get_info(self):
+        author = self.youtube.author
+        title = self.youtube.title
+        desc = self.youtube.description
+        date = self.youtube.publish_date
+
+        views = self.youtube.views
+        length = self.youtube.length
+        rating = self.youtube.rating
--- src/UI.py
+++ src/UI.py
@@ -1,6 +1,29 @@
-import PySimpleGUI
+import PySimpleGUI as sg
+import pytube
 
 
 class UserInterface:
+    # PySimpleGUI.Window(title="YT Downloader", layout=[[]], margins=(500, 300)).read()
 
-    PySimpleGUI.Window(title="YT Downloader", layout=[[]], margins=(500, 300)).read()
+    #
+    layout = [[sg.Text("Insert link")],
+              [sg.Input(key='LINK')],
+              [sg.Text("Insert directory")],
+              [sg.Input(key='DIRECTORY')],
+              [sg.Button('Download'), sg.Button('Close')]]
+
+    #
+    window = sg.Window('YT Downloader', layout)
+
+    while True:
+        event, values = window.read()
+
+        if event == sg.WINDOW_CLOSED or event == 'Close':
+            break
+
+        if event == 'Download':
+            youtube = pytube.YouTube(values['LINK'])
+            video = youtube.streams.get_highest_resolution()
+            video.download(values['DIRECTORY'])
+
+    window.close()
