dad22892a4df052ad1f88f54ce56e1adb9b953d3
VinterSallad <alexander_huynh@icloud.com>
2021-03-10 00:18:53 +0100

    Release stable version 1

--- src/UI.py
+++ src/UI.py
@@ -7,15 +7,16 @@ import pytube
 
 # Main class where UI of downloader is implemented
 class UserInterface:
-    vid_quality_list = ['Max', 'Audio']
+    vid_quality_list = ['Highest', 'Lowest', 'Audio']
+    default_directory = '../Videos'
 
     # Basic Layout of Application
-    layout = [[sg.Text("Insert link")],
-              [sg.Input(key='-LINK-')],
-              [sg.Text("Insert directory")],
-              [sg.Input(key='-DIRECTORY-', readonly=True), sg.FolderBrowse()],
-              [sg.OptionMenu(vid_quality_list, default_value='Max', key='-QUALITY-'),
-               sg.Button('Download'), sg.Button('Preview')]]
+    layout = [[sg.Text("Insert link:")],
+              [sg.Input(key='-LINK-'), sg.Button('Preview')],
+              [sg.Text("Insert directory:")],
+              [sg.Input(key='-DIRECTORY-', readonly=True), sg.FolderBrowse(initial_folder=default_directory)],
+              [sg.OptionMenu(vid_quality_list, default_value='Select Quality', key='-QUALITY-', size=(18, 1)),
+               sg.Button('Download', initial_folder=default_directory, size=(18, 1))]]
 
     # right_column = [[sg.Image('Thumbnail', key='-IMG-')],
     #                 [sg.Text('Length', key='-Length-')],
@@ -26,7 +27,7 @@ class UserInterface:
     #                 [sg.Text('Creator', key='-Creator-')]]
 
     # Initiator
-    # sg.theme()
+    sg.theme('Reddit')
     window = sg.Window('YT Downloader', layout)
 
     # Program loop
@@ -41,8 +42,10 @@ class UserInterface:
             youtube = pytube.YouTube(values['-LINK-'])
 
             # Selected video property control
-            if values['-QUALITY-'] == 'Max':
+            if values['-QUALITY-'] == 'Highest':
                 video = youtube.streams.get_highest_resolution().download(values['-DIRECTORY-'])
+            elif values['-QUALITY-'] == 'Lowest':
+                video = youtube.streams.get_lowest_resolution().download(values['-DIRECTORY-'])
             elif values['-QUALITY-'] == 'Audio':
                 video = youtube.streams.get_audio_only().download(values['-DIRECTORY-'])
 
